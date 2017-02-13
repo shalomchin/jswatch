9d64801ecc1704b50a917a2eea5f6c9bc9734807
Unknown <stanshalomchin@yahoo.com>
2017-02-13 21:38:38 +0800

    Add functions in watch.js

---
+++ js/watch.js
@@ -0,0 +1,15 @@
+function Stopwatch() {
+	var time = 0;
+  var interval;
+  var now;
+  
+  function update() {};
+  function timepassed() {};
+  function timems() {};
+
+  this.isOn = false;
+  this.start = function () {};
+  this.stop = function () {};
+  this.reset = function () {};
+}
+
